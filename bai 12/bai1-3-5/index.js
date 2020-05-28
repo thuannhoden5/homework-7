9bb68001edf7463ef90eb211aa0eaf8bc21ed553
thuannhoden5 <thuannhoden5>
2020-05-28 15:56:40 +0700

    hiihih

---
+++ bai 12/bai1-3-5/index.js
@@ -0,0 +1,8 @@
+// bai 1: 1-d,2-a,3-c,4-b
+// bai 3: khong
+// bai 5:
+const singers = document.getElementsByClassName('singer');
+console.log(singers[1]);
+for (let i = 0; i < singers.length; i++) {
+    console.log(singers[i]);
+}
